38d3a236a57e9bb4aff036fd70d2c4c4e9747a3a
Denzel Wamburu <denzjoseph@gmail.com>
2020-12-22 19:07:03 +0300

    feat: randomUserAgent generator, use commonjs with ESM interoperability

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import userAgents from "./user-agents.json";
-// module.exports = require("./user-agents.json");
+const randomUserAgent =
+  userAgents[Math.floor(Math.random() * userAgents.length)];
 export default userAgents;
+export { randomUserAgent };
